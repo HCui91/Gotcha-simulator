107f8137517d7dc61968eb0eb7bfba6c75f62200
HCui91 <cui980406@gmail.com>
2020-03-10 10:25:02 +0000

    Commenting and readme
    Rewrite the main function, exporting an array of success counts in one trail_number, and do the stats work later.

--- README.md
+++ README.md
@@ -4,9 +4,23 @@
 
 ## Introduction
 
+A simulator for gotcha. 
+
+Probability to meet a shiny pokemon in egg hatching is taken as 1/682. 
+
 ## How to use
+
+Make sure you get `numba` installed. 
+
+Adjust `prob,num_of_trails_max,sample_size` depending on what you are simulating.
+
+## Observation
+Larger `sample_size` eventually ends up with narrower distribution of the mean of success counts. Therefore, don't trust too much comments under the video/article that they got a shiny pokemon in ~ 100 eggs.  
 
 ## File structure
 
+main.py - Function and simulation variables inputs
+
 ## Acknowledgement
 
+Thanks to numba for automatic parallel computing.
--- main.py
+++ main.py
@@ -1,33 +1,65 @@
-import numpy as np 
-import random
-import matplotlib.pyplot as plt
+########################
+# Simulator for gotcha #
+#    Author: HCui91    #
+########################
 
-from numba import njit,prange
+import random
+
+import matplotlib.pyplot as plt
+import numpy as np
+from numba import njit, prange
+
 
 @njit(parallel=True)
-def simulation(prob,num_of_trails_max,sample_size):
-    expt = np.zeros(num_of_trails_max)
-    for num_of_trails in prange(int(num_of_trails_max)):
-        success = 0 # reset counter each new number of trails
-        for sample_index in prange(int(sample_size)):
-            for _ in prange(num_of_trails):
-                if random.random() <= prob:
-                    success += 1
-        expt_of_success = success / sample_size
-        expt[num_of_trails] = expt_of_success
-    return expt
+def simulation(num_of_trails, sample_size, prob):
+    """
+    This program simulate n trails with a given sample size.
+    Compare the random number (0-1) with probability.
+    -----------
+    Inputs:
+    num_of_trails: number of trails in this gotcha
+    sample_size: how many samples are taken
+    prob: the probability to success
+    ----------
+    Outputs:
+    result: the array of success counts in each sample (stats need to be done outside the function)"""
+    # Housekeeping
+    sample_size = int(sample_size)
+    num_of_trails = int(num_of_trails)
+    # Initialise output array
+    result = np.zeros(sample_size)
+    # Loop with prange -> parallel
+    for sample_index in prange(sample_size):
+        success = 0  # reset the counter
+        for _ in prange(num_of_trails):
+            if random.random() <= prob:
+                success += 1
+        result[sample_index] = success
+    return result
+
 
 # function inputs
 prob = 1 / 682
 num_of_trails_max = 3000
-sample_size = 25
+sample_size = 10
 
-# output array
-trail_index = np.arange(0,num_of_trails_max,1)
-expt = simulation(prob,num_of_trails_max,sample_size)
+# Result array initialise
+trail_index = np.arange(0, num_of_trails_max, 1)
+expt = np.zeros(num_of_trails_max)
+expt_min = np.zeros(num_of_trails_max)
 
-#print(len(expt),len(trail_index))
+# Simulation
+for num_of_trails in trail_index:
+    temp = simulation(num_of_trails, sample_size, prob)
+    expt[num_of_trails] = np.mean(temp)
+    expt_min[num_of_trails] = np.min(temp)
+    temp = []
 
-plt.figure(0,figsize=(8,6))
-plt.scatter(trail_index,expt)
+# Plotting
+plt.figure(0, figsize=(8, 6))
+plt.plot(trail_index, expt, label="Mean")
+plt.scatter(trail_index, expt_min, label="Min")
+plt.legend()
+plt.xlabel("Number of trails")
+plt.ylabel("Number of success")
 plt.show()
